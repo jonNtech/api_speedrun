edffe1b99580c19ace76ff4ab57f763f0ffea9d1
jonNtech <31232174+jonNtech@users.noreply.github.com>
2024-08-11 15:03:28 -0700

    batman

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const app = express();
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));  // To parse URL-encoded data
+app.use(express.urlencoded({ extended: true })); // To parse URL-encoded data
 app.use(express.json());
-app.use(methodOverride('_method'));  // Override with POST having ?_method=DELETE/PUT
-app.use(express.static('public'));
+app.use(methodOverride("_method")); // Override with POST having ?_method=DELETE/PUT
+app.use(express.static("public"));
 
 // Connect to MongoDB
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
 }
 
-connectDB()
+connectDB();
 
 // Routes
-const itemsRoute = require('./controllers/item');
-app.use('/api', itemsRoute);
+const itemsRoute = require("./controllers/item");
+app.use("/api", itemsRoute);
 
 // Start the Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
